345c22ae56184f87d2981d025b699e210311ec28
kazumakano <kazumakano@outlook.jp>
2022-05-21 14:51:00 +0900

    update utility.py

--- script/utility.py
+++ script/utility.py
@@ -3,7 +3,7 @@ import math
 import os.path as path
 import pickle
 from datetime import datetime
-from typing import Optional
+from typing import Literal, Optional
 import numpy as np
 import yaml
 from matplotlib import pyplot as plt
@@ -16,15 +16,26 @@ def load_acc_log(file: str) -> tuple[np.ndarray, np.ndarray]:
 
     return np.linalg.norm(data[:, 1:], axis=1), data[:, 0]
 
+def load_ar_log(file: str) -> tuple[np.ndarray, np.ndarray]:
+    pos = np.empty((0, 2), dtype=np.float64)
+    ts = np.empty(0, dtype=datetime)
+
+    with open(file) as f:
+        for row in csv.reader(f):
+            pos = np.vstack((pos, (-np.float64(row[1]), np.float64(row[3]))))
+            ts = np.hstack((ts, datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S.%f")))
+
+    return pos, ts
+
 def load_pose_log(file: str) -> tuple[np.ndarray, np.ndarray]:
     data = np.loadtxt(file, dtype=np.float64, delimiter=",")[:, (0, 1, 3)]    # (timestamp, x, y)
     print(f"{path.basename(file)} has been loaded")
 
     return data[:, 1:], data[:, 0]
 
-def rot(angle: float, pos: np.ndarray) -> np.matrix:
+def rot(angle: float, pos: np.ndarray) -> np.ndarray:
     angle = math.radians(angle)
-    return np.dot(np.matrix(((math.cos(angle), -math.sin(angle)), (math.sin(angle), math.cos(angle))), dtype=np.float64), pos.T).T
+    return np.dot(np.array(((math.cos(angle), -math.sin(angle)), (math.sin(angle), math.cos(angle))), dtype=np.float64), pos.T).T
 
 def loop_closure(pos: np.ndarray) -> np.ndarray:
     err = pos[-1] - pos[0]
@@ -82,13 +93,14 @@ def format_log(file_name: str, offset: float, pos: np.ndarray, ts: np.ndarray, f
 
     print(f"written to {file_name}.pkl")
 
-def write_conf(angle: float, init_pos: tuple[int, int], mag: float, offset: float, file_name: str) -> None:
+def write_conf(angle: float, init_pos: tuple[int, int], mag: float, offset: float, app: Literal["ar", "tango"], file_name: str) -> None:
     with open(path.join(path.dirname(__file__), "../formatted/", file_name + ".yaml"), mode="w") as f:
         yaml.safe_dump({
             "angle": angle,
             "init_pos": init_pos,
             "mag": mag,
-            "offset": offset
+            "offset": offset,
+            "app": app
         }, f)
 
     print(f"written to {file_name}.yaml")
